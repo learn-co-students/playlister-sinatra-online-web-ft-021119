79c0a8a8aab71ddc347c1edf11b31c226fb8db88
bebopvinh <vinhdo07@gmail.com>
2019-04-06 18:50:43 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,25 +22,38 @@ class SongsController < ApplicationController
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
+    @message = "Successfully created song."
     erb :'/songs/show'
   end
 
   post '/songs/:slug/edit' do
-    @artists = Artist.all
-    @genres = Genre.all
     @song = Song.find_by_slug(params[:slug])
     redirect "/songs/#{@song.slug}/edit"
   end
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
-
     erb :'/songs/edit'
   end
 
   patch '/songs/:slug' do
-    binding.pry
     params.delete("_method")
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(name: params[:song][:name])
+
+    unless params[:artist][:name].empty?
+      @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    else
+      @artist = Artist.find_by_name(params[:song][:artist][:name])
+      @song.artist = @artist
+    end
+
+    @genres = params[:genres].map {|g| Genre.find_or_create_by(name: g)}
+    @song.genres.clear
+    @song.genres << @genres
+    @song.save
+    @message = "Successfully updated song."
+    erb :'/songs/show'
   end
 
 end
