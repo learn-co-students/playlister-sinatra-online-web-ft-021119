c14ae5afb18ff0c015ab136739785196738c4d9b
github username <github email address>
2019-04-07 01:14:49 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,25 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
-  extend Slugifiable::ClassMethods
-  include Slugifiable::InstanceMethods
+  # extend Slugifiable::ClassMethods
+  # include Slugifiable::InstanceMethods
+
+
+  def slug
+    @slug = self.name.strip.downcase.gsub(" ","-")
+  end
+
+
+
+  def self.find_by_slug(slug)
+    artist_name = "#{slug.gsub("-"," ")}".split.map(&:capitalize).join(" ")
+
+    self..each do |artist|
+      if artist.name == artist_name
+        @artist = artist
+              #binding.pry
+      end
+    end
+  end
+
 end
--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,13 +1,14 @@
 module Slugifiable
   module InstanceMethods
-    def slug(song_name)
-      song_name.downcase.gsub(" ","-")
-    end
+    # def slug(artist_name)
+    #   slug = artist_name.strip.downcase.gsub(" ","-")
+    # end
   end
 
   module ClassMethods
-    def find_by_slug
-    end
+    # def find_by_slug(slug)
+    #   slug = "%#{slug.chars[0..20]gsub("-"," ")}%"
+    #   found = self.where("name LIKE ?", slug)
+    # end
   end
-
 end
