a3269a1d95d26f0f3191493278de2a0dd18bbffb
github username <github email address>
2019-04-07 14:22:28 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,6 +7,8 @@ class ArtistsController < ApplicationController
 
   get '/artists/:slug' do
     @artist = Artist.find_by_slug(params[:slug])
+    @songs = @artist.songs
+    @genres = @artist.genres
     #binding.pry
     erb :'/artists/show'
   end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,10 +5,39 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
+      if @song
+        @artist = @song.artist
+        @genre = @song.genres
+      end
     #binding.pry
     erb :'/songs/show'
+  end
+
+  post '/songs/new' do
+    binding.pry
+    if !Song.where(name == params[:song])
+      @song = Song.new
+      @song.name = params[:name]
+      @song.artist = params[:artist]
+      @song.genre = params[:genre]
+      @song.save
+    end
+
+    if Artist.where(name == params[:artist])
+      erb :'/songs/:slug'
+    else
+      Artist.create_by(params[:artist])
+    end
+  end
+
+  get 'songs/:slug/edit' do
+    erb :'/songs/edit'
   end
 
 end
