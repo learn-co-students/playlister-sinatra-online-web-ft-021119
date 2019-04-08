3767c57e894f28b18bb55a43f8a434cdc86d3c1d
github username <github email address>
2019-04-08 00:16:19 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,26 +16,32 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    if !Artist.find_by(name: params[:artist][:name])
-      artist = Artist.create(name: params[:artist][:name])
-      @song = Song.create(name: params[:song][:name])#create with name only
-      @song.artist = artist
-    else
-      @song = Song.create(name: params[:song][:name])#create with name only
-      @song.artist = Artist.find_by(name: params[:artist][:name])
-    end
-    redirect "/songs/#{@song.slug}"
+    #binding.pry
+    @song = Song.create(name: params[:song][:name])
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    # if !params["genres"].nil? 
+    #   params["genres"].each do |id| 
+    #     @genre = Genre.find(id) 
+    #     @song.genres << @genre 
+    #   end
+    @song.genre_ids = params[:genres]
+    @song.save
+    flash[:message]="Successfully created song."
+
+    redirect("/songs/#{@song.slug}")
   end
 
-  get 'songs/:slug/edit' do
-    @song = Song.find_by_slug(params[:slug])
 
+  get 'songs/:slug/edit' do
+    binding.pry
+    @song = Song.find_by_slug(params[:slug])
     erb :"/songs/edit"
   end
 
   patch 'songs/:slug/edit' do
     @song.artist = params[:song][:artist] if params[:song][:artist]
     @song.genre = params[:song][:genres] if params[:song][:genres]
+    flash[:message]="Successfully edited song."
     erb :"/songs/#{@song.slug}"
   end
 
--- config.ru
+++ config.ru
@@ -3,6 +3,7 @@ require './config/environment'
 begin
   fi_check_migration
 
+  #use Rack::Flash
   use Rack::MethodOverride
   use ArtistsController
   use SongsController
